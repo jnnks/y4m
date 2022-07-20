c29dc2fb54f45be543d7e0b209382247e29fb1b0
jnnks <ymlu@pm.me>
2022-07-20 19:27:38 +0000

    release v0.1

--- mix.exs
+++ mix.exs
@@ -7,13 +7,28 @@ defmodule Y4m.MixProject do
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
 
   def application, do: []
   defp deps, do: [
-    {:dialyxir, "~> 1.1", only: [:dev], runtime: false},
-    {:nx, "~> 0.1", only: [:test], runtime: false}
+    {:dialyxir, "~> 1.1", only: :dev, runtime: false},
+    {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+    {:nx, "~> 0.1", only: :test, runtime: false}
   ]
+
+  defp description() do
+    "Collection of convenience functions to read *.y4m files."
+  end
+
+  defp package do
+    [
+      files: ~w(lib .formatter.exs mix.exs README* LICENSE*),
+      licenses: ["The Unlicense"],
+      links: %{"GitHub" => "https://github.com/jnnks/y4m"}
+    ]
+  end
 end
